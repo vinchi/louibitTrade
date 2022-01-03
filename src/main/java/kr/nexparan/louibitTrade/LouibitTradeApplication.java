92e16e54c19e2a771d7e577383f37357e9fbd68f
NexParan <vinchi.reo@gmail.com>
2022-01-03 15:45:58 +0900

    포로젝트 생성

---
+++ src/main/java/kr/nexparan/louibitTrade/LouibitTradeApplication.java
@@ -0,0 +1,13 @@
+package kr.nexparan.louibitTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouibitTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouibitTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/nexparan/louibitTrade/LouibitTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.nexparan.louibitTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LouibitTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
