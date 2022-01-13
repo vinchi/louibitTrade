d401e6edf466e9832f82384d36d5f4dc5bbcb809
NexParan <vinchi.reo@gmail.com>
2022-01-13 18:22:14 +0900

    지갑 페이지 구성중

--- src/main/java/kr/nexparan/louibitTrade/LouibitTradeApplication.java
+++ src/main/java/kr/nexparan/louibitTrade/LouibitTradeApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 import org.springframework.context.MessageSource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
